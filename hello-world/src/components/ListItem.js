d255c814331ee863e6fc18b8afd265fc9a0873fe
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-01 10:03:06 +0530

    Styling Components.

--- hello-world/src/components/ListItem.js
+++ hello-world/src/components/ListItem.js
@@ -2,10 +2,10 @@ import AddToCartIcon from "../assets/icons/add_cart.svg";
 
 const ListItem = () => {
   return (
-    <div>
-      <img src="/assets/placeholder.png" alt="some title" />
-      <div>
-        <div>
+    <div className={"item-card"}>
+      <img className={"img-fluid"} src="/assets/placeholder.png" alt="some title" />
+      <div className={"item-card__information"}>
+        <div className={"pricing"}>
           <span>RS 340</span>
           <small>
             <strike>Rs 450</strike>
@@ -15,7 +15,7 @@ const ListItem = () => {
           <h3>Title of the item</h3>
         </div>
       </div>
-      <button>
+      <button className={"cart-add"}>
         <span>Add to cart</span>
         <img src={AddToCartIcon} alt="Cart Icon" />
       </button>
