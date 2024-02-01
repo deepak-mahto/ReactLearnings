029a00da213086f4ae8315c21222fefc0552ec48
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-02 00:03:58 +0530

    Add dynamic component

--- hello-world/src/components/ListItem.js
+++ hello-world/src/components/ListItem.js
@@ -1,18 +1,28 @@
 import AddToCartIcon from "../assets/icons/add_cart.svg";
 
 const ListItem = () => {
+  const data = {
+    discountedPrice: 340,
+    price: 450,
+    title: "Title of the Item",
+    thumbnail:"placeholder.png"
+  };
   return (
     <div className={"item-card"}>
-      <img className={"img-fluid"} src="/assets/placeholder.png" alt="some title" />
+      <img
+        className={"img-fluid"}
+        src="/assets/placeholder.png"
+        alt="some title"
+      />
       <div className={"item-card__information"}>
         <div className={"pricing"}>
-          <span>RS 340</span>
+          <span>Rs {data.discountedPrice}</span>
           <small>
-            <strike>Rs 450</strike>
+            <strike>Rs {data.price}</strike>
           </small>
         </div>
         <div>
-          <h3>Title of the item</h3>
+          <h3>{data.title}</h3>
         </div>
       </div>
       <button className={"cart-add"}>
