2ec6f3b311d8b3810d040d697522593b38cf823a
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-01 07:28:04 +0530

    Alternative way of JSX .

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   // return <div>Hello, World!</div>;
   return React.createElement(
     "div",
-    {},
+    { className: "dummyClass", id: "container" },
     React.createElement("h1", {}, "Hello, World!")
   );
 };
