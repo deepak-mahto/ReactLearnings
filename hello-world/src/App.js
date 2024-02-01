e1bd30e4f48c4414a445a2a130d4745f393cd732
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-01 07:21:19 +0530

    Alternative way.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+
 const App = () => {
-  return <div>Hello, World!</div>;
+  // return <div>Hello, World!</div>;
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Hello, World!")
+  );
 };
 
 export default App;
