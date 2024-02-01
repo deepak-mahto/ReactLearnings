d510737a6107d5aa15a6521fbf60829b1c4466fc
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-02 04:29:20 +0530

    Add header component.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,8 +1,10 @@
 import Products from "./components/Products/products";
+import Header from "./components/Products/Layout/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Products />
     </div>
   );
