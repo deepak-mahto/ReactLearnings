09a44d09e51a920ec3042522b7ca26c287b38271
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-02 04:56:56 +0530

    Add subheader component.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,12 @@
 import Products from "./components/Products/products";
 import Header from "./components/Products/Layout/Header";
+import SubHeader from "./components/Products/Layout/SubHeader";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <SubHeader />
       <Products />
     </div>
   );
