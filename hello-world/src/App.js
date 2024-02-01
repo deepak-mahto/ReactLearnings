fbd0cecc300f6a039a5f2dc988303ededc2f292b
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-02 01:53:47 +0530

    Passing data from parent to child component.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,7 +3,22 @@ import ListItem from "./components/ListItem";
 const App = () => {
   return (
     <div>
-      <ListItem></ListItem>
+      <ListItem
+        data={{
+          discountedPrice: 340,
+          price: 450,
+          title: "Title of the Item 1",
+          thumbnail: "placeholder.png",
+        }}
+      ></ListItem>
+      <ListItem
+        data={{
+          discountedPrice: 320,
+          price: 400,
+          title: "Title of the Item 2",
+          thumbnail: "placeholder.png",
+        }}
+      ></ListItem>
     </div>
   );
 };
--- hello-world/src/components/ListItem.js
+++ hello-world/src/components/ListItem.js
@@ -1,18 +1,12 @@
 import AddToCartIcon from "../assets/icons/add_cart.svg";
 
-const ListItem = () => {
-  const data = {
-    discountedPrice: 340,
-    price: 450,
-    title: "Title of the Item",
-    thumbnail:"placeholder.png"
-  };
+const ListItem = ({ data }) => {
   return (
     <div className={"item-card"}>
       <img
         className={"img-fluid"}
-        src="/assets/placeholder.png"
-        alt="some title"
+        src={`/assets/${data.thumbnail}`}
+        alt={data.title}
       />
       <div className={"item-card__information"}>
         <div className={"pricing"}>
