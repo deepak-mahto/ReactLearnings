291c490bba208253f024424cfe8616258c166bde
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-09 18:24:40 +0530

    Adding todo-app

---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,12 @@
+const TodoList = ({ list }) => {
+  return (
+    <ul>
+      {list &&
+        list.map((item, index) => {
+          return <li key={`${item.id}`}>{item.text}</li>;
+        })}
+    </ul>
+  );
+};
+
+export default TodoList;
