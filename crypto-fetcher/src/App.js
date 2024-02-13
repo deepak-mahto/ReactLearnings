2b33f6037b89e7ca1f4bbc89f5bd8a625f9d8601
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-13 14:02:23 +0530

    Initial commit

---
+++ crypto-fetcher/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Coin from "./components/Coin";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Coin />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ crypto-fetcher/src/components/Coin.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const Coin = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Coin with details.</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Coin;
